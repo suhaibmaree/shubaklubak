9f3ba82eba161edea2483bae7608a34a0b5a426f
suhaib <suhaib.sym.m@gmail.com>
2019-04-02 13:54:17 +0300

    drawer toggle

--- app/src/main/java/edu/mareeaaup/s/shubaklubak/MainActivity.java
+++ app/src/main/java/edu/mareeaaup/s/shubaklubak/MainActivity.java
@@ -2,10 +2,14 @@ package edu.mareeaaup.s.shubaklubak;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     public static final int RC_SIGN_IN = 1;
+    private DrawerLayout drawer;
 
     List<AuthUI.IdpConfig> providers = Arrays.asList(
 
@@ -52,7 +57,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         };
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
+                R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
     }//end on creat
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
+    }
 
     @Override
     protected void onResume() {
