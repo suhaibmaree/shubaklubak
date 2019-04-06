b6eb90c05ea14ee1451f1d12827f69901a0cb868
suhaib <suhaib.sym.m@gmail.com>
2019-04-06 23:19:10 +0300

    Drawer Fragments

--- app/src/main/java/edu/mareeaaup/s/shubaklubak/MainActivity.java
+++ app/src/main/java/edu/mareeaaup/s/shubaklubak/MainActivity.java
@@ -2,11 +2,13 @@ package edu.mareeaaup.s.shubaklubak;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.Toolbar;
@@ -60,13 +62,42 @@ public class MainActivity extends AppCompatActivity {
 
         android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new HomeFragment()).commit();
+                        break;
+
+                    case R.id.modes:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new ModesFragment()).commit();
+                        break;
+
+                    case R.id.users:
+                        Toast.makeText(MainActivity.this,"Users",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                drawer.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
                 R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-    }//end on creat
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.home);
+        }
+
+    }//end on create
 
     @Override
     public void onBackPressed() {
