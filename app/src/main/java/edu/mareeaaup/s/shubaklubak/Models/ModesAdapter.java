35de2134ce64816a16919073991be35633f7ad99
suhaib <suhaib.sym.m@gmail.com>
2019-06-12 01:13:34 +0300

    Add delete icon for mode card

--- app/src/main/java/edu/mareeaaup/s/shubaklubak/Models/ModesAdapter.java
+++ app/src/main/java/edu/mareeaaup/s/shubaklubak/Models/ModesAdapter.java
@@ -1,7 +1,11 @@
 package edu.mareeaaup.s.shubaklubak.Models;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -9,16 +13,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
+import edu.mareeaaup.s.shubaklubak.Fragments.HomeFragment;
+import edu.mareeaaup.s.shubaklubak.Fragments.ModesFragment;
 import edu.mareeaaup.s.shubaklubak.R;
 
 public class ModesAdapter extends RecyclerView.Adapter<ModesAdapter.ViewHolder> {
@@ -47,7 +57,7 @@ public class ModesAdapter extends RecyclerView.Adapter<ModesAdapter.ViewHolder> 
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
+    public void onBindViewHolder(@NonNull final ViewHolder viewHolder, final int i) {
         modecount = 0;
         for (int j = 0; j < modeList.get(i).getDevices().size(); j++) {
             if (modeList.get(i).getDevices().get(j).getState() == Boolean.TRUE)
@@ -115,8 +125,44 @@ public class ModesAdapter extends RecyclerView.Adapter<ModesAdapter.ViewHolder> 
                 modeList.get(i).setExpanded(!expanded);
                 notifyItemChanged(i);
             }
+        });//end instead recycler view
+
+        viewHolder.delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //create Dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setMessage("Do you want to delete this mode ?");
+                builder.setCancelable(true);
+
+                builder.setTitle("Add Mode Name")
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                                FragmentManager manager = ((AppCompatActivity)mContext).getSupportFragmentManager();
+                                mDatabase.child(FirebaseAuth.getInstance().getUid()).child("modes")
+                                        .child(modeList.get(i).getKey()).removeValue();
+                                manager.beginTransaction().replace(R.id.fragment_container,
+                                        new ModesFragment()).commit();
+                                mAdapter.notifyDataSetChanged();
+
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
         });
-    }
+
+
+    }//end onBindeView
 
     @Override
     public int getItemCount() {
@@ -131,6 +177,9 @@ public class ModesAdapter extends RecyclerView.Adapter<ModesAdapter.ViewHolder> 
         View subItem;
         ImageView cardImage;
         ImageView arrowUp;
+        ImageView delete;
+        Switch modeSwitch;
+        View line;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -140,15 +189,22 @@ public class ModesAdapter extends RecyclerView.Adapter<ModesAdapter.ViewHolder> 
             subItem = itemView.findViewById(R.id.sub_item);
             cardImage = itemView.findViewById(R.id.cover_image);
             arrowUp = itemView.findViewById(R.id.arrow_up);
+            delete = itemView.findViewById(R.id.delete_mode_icon);
+            modeSwitch = itemView.findViewById(R.id.mode_switch);
+            line = itemView.findViewById(R.id.first_line);
+
 
         }
 
         private void bind(Moode moode) {
             boolean expanded = moode.isExpanded();
             subItem.setVisibility(expanded ? View.VISIBLE : View.GONE);
+            delete.setVisibility(expanded ? View.VISIBLE : View.GONE);
             modecounte.setVisibility(expanded ? View.GONE : View.VISIBLE);
             cardImage.setVisibility(expanded ? View.GONE : View.VISIBLE);
             arrowUp.setVisibility(expanded ? View.VISIBLE : View.GONE);
+            modeSwitch.setVisibility(expanded ? View.GONE : View.VISIBLE);
+            line.setVisibility(expanded ? View.GONE : View.VISIBLE);
 
         }
 
